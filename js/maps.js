0fdba8715210995a327fa033533127b2233d191d
Luis Ccalluchi Lopez <daprimovaria@outlook.com>
2019-07-08 08:30:47 -0500

    Formulario listo y Mpas

--- js/maps.js
+++ js/maps.js
@@ -1,55 +1,92 @@
 window.onload = () => {
-
-  function initMap() {
-    // The location of Jazvi
-    var jazvi = { lat: -25.344, lng: 131.036 };
-    // The map, centerednad style at Jazvi
-    var map = new google.maps.Map(
-      document.getElementById('map'), {
-        zoom: 4, center: jazvi,
+    
+  let configurarMapa = () => {
+    mapaGoogle = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -16.51423, lng: -70.316524 },
+        zoom: 30,
         styles: [
+          {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+          {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+          {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
           {
-            "featureType": "road.highway",
-            "elementType": "labels.text",
-            "stylers": [
-              {
-                "color": "#ff3c41"
-              }
-            ]
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
           },
           {
-            "featureType": "road.local",
-            "elementType": "geometry.fill",
-            "stylers": [
-              {
-                "color": "#ff3c41"
-              }
-            ]
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
           },
           {
-            "featureType": "transit.line",
-            "elementType": "geometry.fill",
-            "stylers": [
-              {
-                "color": "#ff3c41"
-              }
-            ]
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{color: '#263c3f'}]
           },
           {
-            "featureType": "water",
-            "elementType": "geometry.fill",
-            "stylers": [
-              {
-                "color": "#72c99f"
-              }
-            ]
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#6b9a76'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{color: '#38414e'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#212a37'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#9ca5b3'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{color: '#746855'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#1f2835'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#f3d19c'}]
+          },
+          {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{color: '#2f3948'}]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{color: '#17263c'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#515c6d'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{color: '#17263c'}]
           }
-        ]
-      });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({ position: jazvi, map: map });
-  }
-
-  initMap();
+        ],
+    });
+  };
+ 
+  configurarMapa();
 
 };
